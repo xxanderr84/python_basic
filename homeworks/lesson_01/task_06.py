7991ecce9483e6f3de4697c7994ca742eea80f89
geekbrains <info@geekbrains.ru>
2020-07-12 22:07:16 +0500

    second task complete, second chance

--- homeworks/lesson_01/task_06.py
+++ homeworks/lesson_01/task_06.py
@@ -1,13 +1,1 @@
-while True:
-    user_input = input('Введите количество секунд: ')
-    if user_input.isdigit():
-        break
-    else:
-        print('Ошибка в числе')
-user_input = int(user_input)
-hours_ = int(user_input/3600)
-user_input = user_input - hours_*3600
-minutes_ = int(user_input/60)
-user_input = user_input - minutes_*60
-seconds_ = user_input
-print(f'{hours_:02}:{minutes_:02}:{seconds_:02}')
+
