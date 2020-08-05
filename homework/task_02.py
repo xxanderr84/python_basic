fd1caa0bc84062a24c2cf9bc863fa64baabedb4f
geekbrains <info@geekbrains.ru>
2020-08-05 16:10:14 +0500

    everything is ready, without number 4

--- homework/task_02.py
+++ homework/task_02.py
@@ -1,6 +1,22 @@
-class MyZeroDivision(Exception):
+class MyZeroDivisionError(Exception):
 
     def __init__(self, txt):
         self.txt = txt
 
-    
+
+while True:
+    value_1 = input('Введите делимое: ')
+    value_2 = input('Введите делитель: ')
+    try:
+        value_1 = float(value_1)
+        value_2 = float(value_2)
+        if value_2 == 0:
+            raise MyZeroDivisionError('Zero division error, try another number')
+        print(f'Частное: {value_1/value_2}')
+    except MyZeroDivisionError:
+        print('Плохие числа, попробуйте другие')
+    except ValueError:
+        print('Нужно вводить числа, а рне строки')
+    exit_1 = input('Продолжаем? (Да/Нет): ')
+    if exit_1 == 'Нет':
+        break
