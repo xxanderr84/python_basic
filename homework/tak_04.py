7146e43f81a356f232871d831a13de37745143fc
geekbrains <info@geekbrains.ru>
2020-08-06 10:30:26 +0500

    everything is ready, without number 4

--- homework/tak_04.py
+++ homework/tak_04.py
@@ -1,10 +1,16 @@
+class EquipmentError(Exception):
+
+    def __init__(self, txt: str):
+        self.txt = txt
+
+
 class Equipment:
     name = ''
     __model = ''
     __id = ''
 
-    def __init__(self, name: str, model: str, id: str):
-        self.__id = id
+    def __init__(self, name: str, model: str, inventory: str):
+        self.__id = inventory
         self.name = name
         self.__model = model
 
@@ -20,8 +26,10 @@ class Equipment:
 class Printer(Equipment):
     __print_speed: int
 
-    def __init__(self, name: str, model: str, id: str, speed: int):
-        super().__init__(name, model, id)
+    def __init__(self, name: str, model: str, inventory: str, speed: int):
+        super().__init__(name, model, inventory)
+        if speed < 1:
+            raise EquipmentError('Print speed must be more than 0')
         self.__print_speed = speed
 
     @property
@@ -32,8 +40,10 @@ class Printer(Equipment):
 class Scanner(Equipment):
     __dpi: int
 
-    def __init__(self, name: str, model: str, id: str, dpi: int):
-        super().__init__(name, model, id)
+    def __init__(self, name: str, model: str, inventory: str, dpi: int):
+        super().__init__(name, model, inventory)
+        if dpi < 300:
+            raise EquipmentError('dpi must be more than 300')
         self.__dpi = dpi
 
     @property
@@ -44,8 +54,10 @@ class Scanner(Equipment):
 class Xerox(Equipment):
     __scan_speed: int
 
-    def __init__(self, name: str, model: str, id: str, speed: int):
-        super().__init__(name, model, id)
+    def __init__(self, name: str, model: str, inventory: str, speed: int):
+        super().__init__(name, model, inventory)
+        if speed < 1:
+            raise EquipmentError('Scan speed must be more than 0')
         self.__scan_speed = speed
 
     @property
